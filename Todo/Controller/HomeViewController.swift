c60abec7298a0d4c20536f9923ad48f19e4dbb2b
Makoto <>
2021-09-14 20:48:43 +0900

    ホームのUIを作成、tableView及びセルの追加。ライブラリの追加をしました。

--- Todo/Controller/HomeViewController.swift
+++ Todo/Controller/HomeViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var adImageView: UIImageView!
+    
+    private let homeTableViewCell = "homeTableViewCell"
+    private var textArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "homeTableViewCell")
+    }
+    
+    @IBAction func plus(_ sender: Any) {
+        
         
     }
     
+    @IBAction func close(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func setting(_ sender: Any) {
+        
+        
+    }
+}
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "homeTableViewCell") as! HomeTableViewCell
+        
+        return cell
+    }
 }
---
+++ Todo/View/HomeTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeTableViewCell.swift
+//  Todo
+//
+//  Created by Makoto on 2021/09/14.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var checkImageView: UIButton!
+    @IBOutlet weak var taskLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
